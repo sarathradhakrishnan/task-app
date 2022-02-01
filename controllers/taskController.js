86dbe79466fc704d8ab27ecd96a64c95d9dd12eb
Sarath Radhakrishnan <sarathrk1@outlook.com>
2022-02-01 21:02:23 +0530

    Added routes and setting up a UI for lists.

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,7 @@
 const express = require('express')
 var router = express.Router()
+const mongoose = require('mongoose')
+const task = mongoose.model('Task')
 
 router.get('/',(req,res)=>{
     res.render('task/addEdit',{
@@ -7,4 +9,25 @@ router.get('/',(req,res)=>{
     })
 })
 
+router.post('/',(req,res)=>{
+    addTask(req,res)
+})
+//function to add task
+
+function addTask(req,res){
+    var Task = new task();
+    Task.taskName = req.body.taskName;
+    Task.taskDesc = req.body.taskDesc;
+    Task.save((err,docs)=>{
+        if(!err){
+            res.redirect("task/list");
+        }
+        else{
+            console.log("Error while saving"+err);
+        }
+    })
+}
+router.get('/list',(req,res)=>{
+    res.json("List Page")
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express')
 const taskController = require('./controllers/taskController')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser')
 
 var app = express();
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
+
 app.set('views',path.join(__dirname,'/views/'))
 app.engine('hbs',exphbs.engine({extname:'hbs',defaultLayout:'mainLayout',layputsDir: __dirname + '/views/layouts'}))
 app.set('view engine','hbs')
